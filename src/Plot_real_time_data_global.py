5e8a91a455de72e665ea48f096c965119f27b76a
harsha5 <harshbitsp@gmail.com>
2019-03-31 16:55:44 -0400

    Graph plots updated for mean value

--- src/Plot_real_time_data_global.py
+++ src/Plot_real_time_data_global.py
@@ -6,36 +6,41 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 from matplotlib import style
 from sensor_msgs.msg import Range
+from statistics import mean
 import pdb
 '''
 This node subscribes to the topic 'range', gets it's values from . This data is then plotted real time 
 '''
 
 fig,ax1 = plt.subplots(1)
+ax1.set_title(r'Range VS Time')
 #ani = 1
 range_values = []
 time_values = []
+max_value = -1
 
 def Range_callback(distance):
 	global range_values
 	global time_values
+	global max_value
 	range_values.append(distance.range)
-	#rospy.loginfo(__dict__.distance.header.stamp)
+	if(distance.range>max_value):
+		max_value = distance.range
 	time_values.append(distance.header.stamp.secs)
-	#time_values.append(distance.header.stamp)
-	#rospy.loginfo(time_values)
 
 def animate(frames):
 	global ax1
 	global range_values
 	global time_values
+	global max_value
 	rospy.loginfo("In Animate \n")	
-	#rospy.loginfo(frames)
 	if(len(range_values)>0):
-		#rospy.loginfo(type(range_values[0]))
-		#rospy.loginfo(time_values[frames])	
-		#x_axis = np.arange(len(range_values))
-		ax1.plot(time_values, range_values)
+		average = mean(range_values) #This can be optimised by keeping a running average rather than computing evry single time.
+		string_to_display_on_graph = 'Mean: ' + str(round(average,2))  #Add appropriate units
+		#rospy.loginfo(string_to_display_on_graph)
+		ax1.clear()
+		ax1.plot(time_values, range_values,color='blue')
+		ax1.annotate(string_to_display_on_graph,xy=(0.5, 0.95), xycoords="axes fraction")
 
 if __name__ == '__main__':
 	rospy.init_node('range_plot', anonymous=True)
@@ -44,6 +49,8 @@ if __name__ == '__main__':
 	rospy.loginfo("In Main \n")
 	ani = animation.FuncAnimation(fig,animate,frames = None,interval = 50)
 	while not rospy.is_shutdown():
+		plt.xlabel('time(s)')
+		plt.ylabel('Range(m)')
 		plt.show()
 		rate.sleep()
 
