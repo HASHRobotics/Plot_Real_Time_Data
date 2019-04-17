fc978bbcb725a08ab43797f22c42536a862b56a6
harsha5 <harshbitsp@gmail.com>
2019-04-16 21:29:51 -0400

    transform_frames.py edited for bearing

--- src/transform_frames.py
+++ src/transform_frames.py
@@ -8,14 +8,14 @@ from matplotlib import style
 from sensor_msgs.msg import Range
 from statistics import mean
 from nav_msgs.msg import Odometry
+from bearing_estimator.msg import bearing_msg
 import pdb
 from math import cos,sin,radians
 
 '''
 Notes to self:
 1) The time for range,bearing and odom is kept same as of now. It should be kept separate as range & bearing are not continous. 
-In the final version of the program we will simply subscribe to a topic which gives intermittent data and that will be plotted.
-As of now if you use a common time, the code should give an error.
+2) Note: A different time for range and bearing is maintained, since it's quite possible that bearing might not get captured or range might not get captured sometimes.
 
 '''
 
@@ -26,7 +26,8 @@ range_values = []
 range_count = 0 	#to see how any entries range values has and then plot only when new values come
 bearing_values = []
 bearing_count = 0	#to see how any entries bearing values has and then plot only when new values come
-time_values = []
+range_time_values = []
+bearing_time_values = []
 odom_Y_values = []
 odom_X_values = []
 max_value = -1
@@ -34,20 +35,27 @@ H_rover12rtk = np.identity(3)
 H_rover22rtk = np.identity(3)
 
 def Range_callback(distance):
-	global range_count
 	global range_values
-	global time_values 
+	global range_time_values 
 	global max_value
-	range_values.append(distance.range)
+	range_values.append(distance.range + random.randint(1,10))
 	if(distance.range>max_value):
 		max_value = distance.range
-	time_values.append(distance.header.stamp.secs)
+	range_time_values.append(distance.header.stamp.secs)
+
+def Bearing_callback(bearing):
+	global bearing_values
+	global bearing_time_values
+	bearing_values.append(bearing.bearing)
+	bearing_time_values.append(bearing.header.stamp.secs)
+
 
 def odom_callback(odom):
 	global odom_Y_values
 	global odom_X_values
 	odom_Y_values.append((odom.pose.pose.position.y) + random.randint(1,10))
-	odom_X_values.append((odom.pose.pose.position.x) + random.randint(1,20))
+	odom_X_values.append(odom.pose.pose.position.x) 
+
 
 # def rtk_callback(rtk):
 # 	# global odom_Y_values
@@ -66,7 +74,8 @@ def animate(frames):
 	global ax2
 	global ax3
 	global range_values
-	global time_values
+	global range_time_values
+	global bearing_time_values
 	global odom_Y_values
 	global odom_X_values
 	global bearing_values
@@ -78,11 +87,18 @@ def animate(frames):
 		range_count+=1
 		average = mean(range_values) #This can be optimised by keeping a running average rather than computing evry single time.
 		string_to_display_on_graph = 'Mean: ' + str(round(average,2))  #Add appropriate units
-		#rospy.loginfo(string_to_display_on_graph)
+		rospy.loginfo(time_values)
+		#rospy.loginfo(range_count)
 		ax1.clear()
-		ax1.plot(time_values, range_values,color='blue')
+		ax1.scatter(range_time_values, range_values,color='blue')
 		ax1.annotate(string_to_display_on_graph,xy=(0.5, 0.9), xycoords="axes fraction")
 		#max value can also be plotted. max_value is being maintained
+
+	if(len(bearing_values)>bearing_count):
+		bearing_count+=1
+		rospy.loginfo("In bearing plot!")
+		ax2.clear()
+		ax2.scatter(bearing_time_values, bearing_values,color='green')
 
 	if(len(odom_X_values)>0):
 		#ax1.clear()
@@ -97,8 +113,8 @@ def set_axis_labels():
 	ax1.set_xlabel('Time')
 	ax2.set_ylabel('Bearing')
 	ax2.set_xlabel('Time')
-	ax3.set_ylabel('Rover Position')
-	ax3.set_xlabel('Time')
+	ax3.set_ylabel('Rover Position in Y axis')
+	ax3.set_xlabel('Rover Position in X axis')
 	# ax1.set_title(r'Range VS Time')
 	# ax2.set_title(r'Bearing VS Time')
 	# ax3.set_title(r'Rover Position VS Time')
@@ -115,10 +131,10 @@ def getH(rtk_x,rtk_y,rover,odom_x = 0,odom_y = 0):
 
 if __name__ == '__main__':
 	rospy.init_node('transform-frames', anonymous=True)
-    # rover1_start_angle = rospy.get_param("rover1_start_angle")
-    # rover2_start_angle = rospy.get_param("rover2_start_angle")
-	rospy.Subscriber("range", Range, Range_callback)
-	rospy.Subscriber("/ak1/odometry/filtered", Odometry, odom_callback)
+	rospy.Subscriber("/range", Range, Range_callback)
+	#rospy.Subscriber("/ak1/odometry/filtered", Odometry, odom_callback)
+	rospy.Subscriber("/odom1", Odometry, odom_callback)
+	rospy.Subscriber("/bearing", bearing_msg, Bearing_callback)
 	rover1_start_angle = rospy.get_param("/Real_time_Plotting/transform_frames/rover1_start_angle")
 	rover2_start_angle = rospy.get_param("/Real_time_Plotting/transform_frames/rover2_start_angle")
 	rate = rospy.Rate(10)
